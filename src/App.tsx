b84689d179003ad77608a619ebee5b0701dec6df
[dyad] <muppallasaiprasanna@gmail.com>
2025-08-04 06:06:55 +0530

    Reverted all changes back to version bc5a78ad397d67a6830ebf5503c3a6d16d9e9951

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,14 @@ import Index from './pages/Index';
 import About from './pages/About';
 import CategoryPage from './pages/CategoryPage';
 import NotFound from './pages/NotFound';
-import Login from './pages/Login';
-import Admin from './pages/Admin';
-import ProtectedRoute from './components/ProtectedRoute';
 
 const App = (): JSX.Element => {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Public Routes */}
         <Route path="/" element={<Index />} />
         <Route path="/about" element={<About />} />
         <Route path="/:category" element={<CategoryPage />} />
-        <Route path="/login" element={<Login />} />
-        
-        {/* Protected Admin Route */}
-        <Route 
-          path="/admin"
-          element={
-            <ProtectedRoute>
-              <Admin />
-            </ProtectedRoute>
-          } 
-        />
-
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,10 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './globals.css';
 import { ThemeProvider } from "@/components/ThemeProvider";
-import { AuthProvider } from '@/contexts/AuthContext';
 
 const root = createRoot(document.getElementById('root')!);
 root.render(
   <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <App />
   </ThemeProvider>
 );
